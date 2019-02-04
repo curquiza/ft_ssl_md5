1a0987ed35f28609849ff128bb4be92f5e149075
Clementine Urquizar <clementine.urquizar@gmail.com>
2019-02-04 01:42:21 +0100

    little fix sha256_init.c -> SHA256 instead of MD5

--- src/sha256_init.c
+++ src/sha256_init.c
@@ -20,12 +20,12 @@ t_ex_ret	message_padding_sha256(t_sha256 *data)
 	size_t		tmp_len;
 	uint64_t	msg_len_bits;
 
-	tmp_len = (data->msg_len + 1 + MD5_MSG_LEN_BYTES);
-	if (tmp_len % (MD5_CHUNK_BYTES) == 0)
+	tmp_len = (data->msg_len + 1 + SHA256_MSG_LEN_BYTES);
+	if (tmp_len % (SHA256_CHUNK_BYTES) == 0)
 		data->padded_msg_len = tmp_len;
 	else
-		data->padded_msg_len = (tmp_len / (MD5_CHUNK_BYTES) + 1)
-					* (MD5_CHUNK_BYTES);
+		data->padded_msg_len = (tmp_len / (SHA256_CHUNK_BYTES) + 1)
+					* (SHA256_CHUNK_BYTES);
 	if (!(data->padded_msg = (t_byte *)ft_memalloc(data->padded_msg_len)))
 		return (FAILURE);
 	/* ft_printf("padded_msg len = %d = 0x%x\n", data->padded_msg_len, data->padded_msg_len); //DEBUG */
